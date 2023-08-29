dd3187e30d93b96dc5ced1a79d78c36b1e13fcee
Aldo-C <61980808+Aldo-C@users.noreply.github.com>
2023-08-28 20:40:48 -0500

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+ReactDOM.render(
+  <div>
+    <h1>Hello!</h1>
+    <ul>
+      <li>Bacon</li>
+      <li>Cheeks</li>
+      <li>Clapper</li>
+    </ul>
+  </div>,
+  document.getElementById("root")
+);
